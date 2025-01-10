b14d5e3e2e4843212f43c38c9b9f14c7ca74d623
Jayvan <jayvanramos23@gmail.com>
2025-01-10 13:53:57 -0400

    fixing shell.c

--- shell.c
+++ shell.c
@@ -15,10 +15,7 @@ int main(void) {
     pid_t pid;
     int i, j;
     char cwd[1024];
-    char **env;
-    char *var;
-    char *value;
-
+    int status = 0;  /* Track the exit status of the last command */
 
     while (1) {
         i = 0;  /* Declare variables at the top */
@@ -32,7 +29,7 @@ int main(void) {
 
         line[nread - 1] = '\0';  /* Remove newline character */
         args[i] = strtok(line, " ");
-        while (args[i] != NULL) {
+        while (args[i] != NULL && i < 99) {
             i++;
             args[i] = strtok(NULL, " ");
         }
@@ -42,8 +39,12 @@ int main(void) {
 
         /* Built-in: exit */
         if (strcmp(args[0], "exit") == 0) {
+            if (args[1] != NULL) {  /* If there are extra arguments after exit */
+                fprintf(stderr, "exit: too many arguments\n");
+                continue;  /* Print error and continue with the prompt */
+            }
             free(line);
-            exit(0);
+            exit(status);  /* Exit the shell with the current status */
         }
 
         /* Built-in: cd */
@@ -79,7 +80,7 @@ int main(void) {
         /* Built-in: env */
         if (strcmp(args[0], "env") == 0) {
             /* Directly use environ to access the environment variables */
-            for (*env = environ; *env != NULL; env++) {
+            for (char **env = environ; *env != NULL; env++) {
                 printf("%s\n", *env);
             }
             continue; /* Avoid further processing */
@@ -90,8 +91,8 @@ int main(void) {
             if (args[1] == NULL || strchr(args[1], '=') == NULL) {
                 fprintf(stderr, "export: usage: export VAR=value\n");
             } else {
-                var = strtok(args[1], "=");
-                value = strtok(NULL, "=");
+                char *var = strtok(args[1], "=");
+                char *value = strtok(NULL, "=");
                 if (setenv(var, value, 1) != 0) {
                     perror("export");
                 }
@@ -102,17 +103,40 @@ int main(void) {
         /* Execute external commands */
         pid = fork();
         if (pid == 0) { /* Child process */
-            if (execve(args[0], args, (char * const*)environ) == -1) {  /* Cast environ to correct type */
-                perror("./hsh");
+            /* Handle commands by checking if the path is absolute */
+            if (args[0][0] != '/') {
+                // Command isn't an absolute path, try to find it in $PATH
+                char *path = getenv("PATH");
+                char *cmd_path = malloc(strlen(path) + strlen(args[0]) + 2);
+                char *tok = strtok(path, ":");
+                while (tok) {
+                    sprintf(cmd_path, "%s/%s", tok, args[0]);
+                    if (access(cmd_path, X_OK) == 0) {
+                        execve(cmd_path, args, environ);
+                    }
+                    tok = strtok(NULL, ":");
+                }
+                free(cmd_path);
+            } else {
+                /* Command is an absolute path, just execute */
+                if (execve(args[0], args, environ) == -1) {
+                    perror("./hsh");
+                    exit(EXIT_FAILURE);  /* Exit with failure status if execve fails */
+                }
             }
-            exit(EXIT_FAILURE);
         } else if (pid < 0) {
             perror("fork");
         } else { /* Parent process */
-            wait(NULL);
+            wait(&status);  /* Wait for child process to finish */
+            if (WIFEXITED(status)) {
+                status = WEXITSTATUS(status);  /* Capture exit status of the child process */
+            } else if (WIFSIGNALED(status)) {
+                status = 128 + WTERMSIG(status);  /* If child process was terminated by a signal */
+            }
         }
     }
 
     free(line);  /* Free memory after loop ends */
     return 0;
 }
+
